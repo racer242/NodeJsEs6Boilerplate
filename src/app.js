aa2d12f81d2c2f3d5d2cccb6a3b22d9219bea38d
racer242 <racer242@gmail.com>
2022-04-20 12:38:21 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+
+import path from 'path';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    res.json({ status: true, message: "Our node.js app works" })
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.end(err.message);
+});
+
+
+export default app;
